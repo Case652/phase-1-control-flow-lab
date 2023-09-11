3888b6901693f1aa6616897133847b8994efea1f
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-11 11:46:14 -0500

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet){
   // Write your code here!
+  if (Feet <= 400){
+      return `This one is on me!`
+  } else if(Feet <=2000) {
+      return `That will be twenty bucks.`
+  } else if(Feet <=2500){
+      return `I will gladly take your thirty bucks.`
+  } else {
+      return `No can do.`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === `NYC`?`Ok, sounds good.` :`No go.`
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case `generous`:
+      return `Thank you so much.`
+    case `not as generous`:
+      return `Thank you.`
+    default:
+      return `Bye.`
+  }
 }
